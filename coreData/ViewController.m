d34518a802f0cdc00735aaf3a78b9802debb6599
liuzhecodekiller <qianfeng@qianfengdeMacBook-Pro-liuzhe.local>
2015-08-08 19:26:19 +0800

    Initial Commit

---
+++ coreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  coreData
+//
+//  Created by qianfeng on 15-8-8.
+//  Copyright (c) 2015年 LZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
